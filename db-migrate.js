b20b0f814507d4efdd89af1e699fec1c091e14c5
Xristianx <basinillocj5@gmail.com>
2026-07-16 00:45:18 +0800

    chore: add eslint directive for require statements in db-migrate.js

--- db-migrate.js
+++ db-migrate.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const postgres = require('postgres');
 require('dotenv').config({ path: '.env' });
 require('dotenv').config({ path: '.env.local' });
